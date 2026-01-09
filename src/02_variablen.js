b1c88f9cfbf38f59db09bb08da74de9392bc773c
Peter Schmidt <schmidt.multimedia@gmail.com>
2026-01-09 08:42:21 +0100

    Add files via upload

---
+++ src/02_variablen.js
@@ -0,0 +1,35 @@
+// Zeilenkommentar  | Strg + #
+
+/**
+ * 
+ *  Block-Kommentar
+ * 
+ * 
+ * 
+ */
+
+/***** Variablen 01 *******/
+
+
+
+/* Deklaration + Wertzuweisung I */
+
+
+
+
+
+
+
+/*  
+    Wertzuweisung II + Datentypen 
+    JS ist ein untypisierte Sprache!
+*/
+
+
+
+
+
+
+
+/* Variablen vs. Konstanten in JS */
+
